4e6fe113d68dc29cdf97840fd38ebe43b5ad274b
MentalFS <git@mentalfs.de>
2020-02-09 00:26:21 +0100

    Option to execute a command only when the bot is idle

--- nightly-daemon.sh
+++ nightly-daemon.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-BUILD="last monday" # for really daily updates: --build today
-
 set -e
 { for COMMAND in timeout; do
 	which "$COMMAND" >/dev/null || { echo "Could not find $COMMAND in PATH." 1>&2; exit 1; } ; done }
 cd "$(dirname "$(readlink -f "$0")")"
 exec 200>lock
-
-# find logs/pointSystem/ -type f -mmin -60 | egrep -q . || echo EXIT
 
 function startup() {
 	echo START
@@ -19,7 +15,13 @@ function command() {
 		exit 1
 	fi
 
+	if (($WHEN_IDLE)) && find logs/pointSystem/ -type f -mmin -60 | egrep -q . ; then
+		echo  "Channel is still active." >&2
+		exit 1
+	fi
+
 	echo "$COMMAND" > fifo
+	timeout 30s tail -f logs/service.log
 }
 
 function update() {
@@ -28,7 +30,9 @@ function update() {
 }
 
 function read_parameters() {
+	BUILD="last monday"
 	NO_UPDATE=0
+	WHEN_IDLE=0
 
 	while [[ "$1" == -* ]] ; do
 		case "$1" in
@@ -38,6 +42,9 @@ function read_parameters() {
 			"--build")
 				BUILD="$2"
 				shift
+				;;
+			"--when-idle")
+				WHEN_IDLE=1
 				;;
 			"--")
 				shift
