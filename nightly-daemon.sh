4dbe69b7251654e4dc93cfc00cddd54d3ca9c0fa
MentalFS <git@mentalfs.de>
2020-02-09 15:55:21 +0100

    fixed output redirection message

--- nightly-daemon.sh
+++ nightly-daemon.sh
@@ -13,8 +13,8 @@ function prepare() {
 	test -f nightly-daemon.2.log && mv -f nightly-daemon.2.log nightly-daemon.3.log
 	test -f nightly-daemon.1.log && mv -f nightly-daemon.1.log nightly-daemon.2.log
 	test -f nightly-daemon.log && mv -f nightly-daemon.log nightly-daemon.1.log
-	exec &>nightly-daemon.log
 	(($SILENT)) || echo Output is redirected to $PWD/nightly-daemon.log
+	exec &>nightly-daemon.log
 	exec 2>&1
 }
 
